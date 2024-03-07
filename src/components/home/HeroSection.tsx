f81acfeb04c1f24da985259916a8bf9d2f775953
delabahmani <delabahmani@gmail.com>
2024-03-07 16:07:04 -0500

    Implemented about section, and touch ups to navbar.

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -5,10 +5,10 @@ import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
-    <section>
+    <section className="mt-10">
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+        <div className="col-span-10 place-self-center text-center sm:text-left">
+          <h1 className="text-[#DC7FBE] mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
               hi, i&apos;m{" "}
             </span>
@@ -32,25 +32,14 @@ const HeroSection = () => {
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-btn-grad hover:bg-slate-200 text-[#405169] font-semibold">
               Hire Me
             </button>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-600 hover:bg-slate-800 text-white mt-3">
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2 font-semibold">
                 Download CV
               </span>
             </button>
-          </div>
-        </div>
-        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/images/hero-img.png"
-              alt="brunette programming at desk"
-              className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
-            />
           </div>
         </div>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "custom-gradient": "var(--bg-gradient)",
+        "gradient-2": "var(--bg-grad)",
+        "button-gradient": "var(--button-gradient)",
+        "btn-grad": "var(--btn-home)",
       },
     },
   },
