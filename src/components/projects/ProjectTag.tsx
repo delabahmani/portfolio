a1358b4ce3b1911c8ae70ca00b78be0f3343c2c6
delabahmani <delabahmani@gmail.com>
2024-03-09 15:16:37 -0500

    Finished remainder of Projects section

--- src/components/projects/ProjectTag.tsx
+++ src/components/projects/ProjectTag.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const ProjectTag = () => {
-  return (
-    <div>ProjectTag</div>
-  )
+type ProjectTagProps = {
+  name: string;
+  isSelected: boolean;
+  onClick: (tagName: string) => void;
 }
 
-export default ProjectTag
+const ProjectTag = ({ name, onClick, isSelected } :ProjectTagProps) => {
+  const buttonStyles = isSelected
+    ? "text-white bg-purple-500"
+    : "text-[ADB7BE] border-slate-600 hover:border-white";
+  return (
+    <button
+      onClick={() => onClick(name)}
+      className={` ${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
+    >
+      {name}
+    </button>
+  );
+};
+
+export default ProjectTag;
