66b074defe1e60a5024288a566c66b36f698be7e
delabahmani <delabahmani@gmail.com>
2024-03-20 15:06:27 -0400

    Added previously unsaved files

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,24 +1,25 @@
-import ReactDOMServer from 'react-dom/server';
 import { EmailTemplate } from '@/components/contact/EmailTemplate';
+import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const reactComponent = (
+  <div>
+    <p>Email Body</p>
+  </div>
+);
 
 export async function POST() {
   try {
-    // Render your React component to an HTML string
-    const emailHtml = ReactDOMServer.renderToString(EmailTemplate({ firstName: 'John' }));
-
     const data = await resend.emails.send({
-      from: 'Dela <bahmanidela@gmail.com>',
-      to: ['bahmanidela@gmail.com'],
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
       subject: 'Hello world',
-      text: 'This is a plain text version of the email.', // Provide a plain text version of the email
-      html: emailHtml, // Use the rendered HTML string
+      react: EmailTemplate({ firstName: 'John' }),
     });
 
-    return Response.json(data);
+    return NextResponse.json(data);
   } catch (error) {
-    return Response.json({ error });
+    return NextResponse.json({ error });
   }
 }
